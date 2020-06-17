da5c8b02f70c807bf324bf32d302490c291a6537
Alex <alex.trindade@live.com>
2020-06-17 13:39:55 -0300

    Implementação da api e rotas para página de cadastro e página principal

--- src/Pages/SignUp/index.js
+++ src/Pages/SignUp/index.js
@@ -3,90 +3,95 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
-import Logo from "../../Images/logo-future-eats-invert.svg"
-import { PrincipalButton } from '../../Themes'
-import Container from '@material-ui/core/Container'
+import Logo from "../../Images/logo-future-eats-invert.svg";
+import { PrincipalButton } from "../../Themes";
+import Container from "@material-ui/core/Container";
 
-const useStyles = makeStyles(theme => ({
-    logo: {
-        paddingTop: "24px",
-        paddingBottom: "16px"
-    },
-    title: {
-        padding: "12px"
-    },
-  }));
+const useStyles = makeStyles((theme) => ({
+  logo: {
+    paddingTop: "24px",
+    paddingBottom: "16px",
+  },
+  title: {
+    padding: "12px",
+  },
+}));
 
 const SignUp = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return(
-          <Container fixed>
-            <img src={Logo} className={classes.logo}/>
-            <Typography align="center">Cadastrar</Typography>
-            <form>
-            <Grid container spacing={2} direction="column">
-                <Grid item xs={12}>
-                    <TextField
-                      fullWidth
-                      required
-                      variant="outlined"
-                      label="Nome"
-                      placeholder="Nome e sobrenome"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                      fullWidth
-                      required
-                      variant="outlined"
-                      label="E-mail"
-                      placeholder="email@email.com"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                      fullWidth
-                      required
-                      variant="outlined"
-                      label="CPF"
-                      placeholder="000.000.000-00"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                      type="password"
-                      fullWidth
-                      required
-                      variant="outlined"
-                      label="Senha"
-                      placeholder="Mínimo 6 caracteres"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                      type="password"
-                      fullWidth
-                      required
-                      variant="outlined"
-                      label="Confirmar"
-                      placeholder="Confirma senha anterior"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                  <PrincipalButton
-                    variant="contained"
-                    fullWidth
-                    disableElevation
-                    type="submit"
-                    color="primary">
-                    Criar
-                  </PrincipalButton>
-                    </Grid>
-            </Grid>
-            </form>
-        </Container>
-    )
-}
+  return (
+    <Container fixed>
+      <img src={Logo} alt={"logo aplicativo"} className={classes.logo} />
+      <Typography align='center'>Cadastrar</Typography>
+      <form>
+        <Grid container spacing={2} direction='column'>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              required
+              variant='outlined'
+              label='Nome'
+              placeholder='Nome e sobrenome'
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              required
+              variant='outlined'
+              label='E-mail'
+              placeholder='email@email.com'
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              required
+              variant='outlined'
+              label='CPF'
+              placeholder='000.000.000-00'
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              type='password'
+              fullWidth
+              required
+              variant='outlined'
+              label='Senha'
+              placeholder='Mínimo 6 caracteres'
+              inputProps={{
+                pattern: "/^.{6,}$/",
+                title: "A senha deve conter no mínimo 6 caracteres",
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              type='password'
+              fullWidth
+              required
+              variant='outlined'
+              label='Confirmar'
+              placeholder='Confirma senha anterior'
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <PrincipalButton
+              variant='contained'
+              fullWidth
+              disableElevation
+              type='submit'
+              color='primary'
+            >
+              Criar
+            </PrincipalButton>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+};
 
-export default SignUp
+export default SignUp;
