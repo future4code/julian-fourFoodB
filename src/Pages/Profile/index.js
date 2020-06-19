df03cad7c747104c112a6ac36d93a7cabb8c346c
Alex <alex.trindade@live.com>
2020-06-19 13:35:38 -0300

    Integração da página de endereço com a api

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -1,82 +1,98 @@
-import React, {useState, useEffect} from 'react'
-import { useProtectedPage } from '../../Hooks/ProtectedPage'
-import { ProfileContainer, TitleBar, PersonalDataContainer, InfoContainer, EditButtonContainer, 
-P, AddressContainer, DescriptionAddress, InfoAddress, Address, HistoricContainer, Historic } from '../Profile/style'
-import EditIcon from '@material-ui/icons/Edit';
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import { useProtectedPage } from "../../Hooks/ProtectedPage";
+import {
+  ProfileContainer,
+  TitleBar,
+  PersonalDataContainer,
+  InfoContainer,
+  EditButtonContainer,
+  P,
+  AddressContainer,
+  DescriptionAddress,
+  InfoAddress,
+  Address,
+  HistoricContainer,
+  Historic,
+} from "../Profile/style";
+import EditIcon from "@material-ui/icons/Edit";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 /* import PersonIcon from '@material-ui/icons/Person';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import HomeIcon from '@material-ui/icons/Home'; */
 
 const Profile = () => {
-    useProtectedPage() 
+  const history = useHistory();
+  useProtectedPage();
 
-    let token = localStorage.getItem('token')
+  let token = localStorage.getItem("token");
 
-    if(token === null){
-        token = sessionStorage.getItem('token')
-    }
+  if (token === null) {
+    token = sessionStorage.getItem("token");
+  }
 
-    const [userData, setUserData] = useState([])
-    const [orderHistory, setOrderHistory] = useState([])
+  const [userData, setUserData] = useState([]);
+  const [orderHistory, setOrderHistory] = useState([]);
 
-    useEffect(() => {
-        getProfile()
-    })
+  useEffect(() => {
+    getProfile();
+  }, []);
 
-    const getProfile = () => {
-        axios
-            .get('https://us-central1-missao-newton.cloudfunctions.net/fourFoodB/profile', {
-                headers: {
-                    auth: token
-                }
-            })
-            .then(response => {
-                setUserData(response.data.user)
-            })
-            .catch(err => {
-                window.alert(err)
-            })
-    }
+  const getProfile = () => {
+    axios
+      .get(
+        "https://us-central1-missao-newton.cloudfunctions.net/fourFoodB/profile",
+        {
+          headers: {
+            auth: token,
+          },
+        }
+      )
+      .then((response) => {
+        setUserData(response.data.user);
+      })
+      .catch((err) => {
+        window.alert(err);
+      });
+  };
 
-    const getHistory = () => {
-        axios
-            .get('https://us-central1-missao-newton.cloudfunctions.net/fourFoodB/orders/history')
-    }
-    return(
-        <ProfileContainer>
-            <TitleBar>Meu Perfil</TitleBar>
+  const getHistory = () => {
+    axios.get(
+      "https://us-central1-missao-newton.cloudfunctions.net/fourFoodB/orders/history"
+    );
+  };
+  return (
+    <ProfileContainer>
+      <TitleBar>Meu Perfil</TitleBar>
 
-            <PersonalDataContainer>
-                <InfoContainer>
-                    <P>{userData.name}</P>
-                    <P>{userData.email}</P>
-                    <P>{userData.cpf}</P>
-                </InfoContainer>
-                <EditButtonContainer>
-                    <EditIcon/>
-                </EditButtonContainer>
-            </PersonalDataContainer>
+      <PersonalDataContainer>
+        <InfoContainer>
+          <P>{userData.name}</P>
+          <P>{userData.email}</P>
+          <P>{userData.cpf}</P>
+        </InfoContainer>
+        <EditButtonContainer>
+          <EditIcon />
+        </EditButtonContainer>
+      </PersonalDataContainer>
 
-            <AddressContainer>
-                <InfoAddress>
-                    <DescriptionAddress>Endereço Cadastrado</DescriptionAddress>
-                    <Address>{userData.address}</Address>
-                </InfoAddress>
-                <EditButtonContainer>
-                    <EditIcon/>
-                </EditButtonContainer>
-            </AddressContainer>
+      <AddressContainer>
+        <InfoAddress>
+          <DescriptionAddress>Endereço Cadastrado</DescriptionAddress>
+          <Address>{userData.address}</Address>
+        </InfoAddress>
+        <EditButtonContainer onClick={() => history.push("/novoendereco")}>
+          <EditIcon />
+        </EditButtonContainer>
+      </AddressContainer>
 
-            <HistoricContainer>
-                <P>Histórico de Pedidos</P>
-            </HistoricContainer>
-            
-            <Historic>
-            Você não realizou nenhum pedido    
-            </Historic> 
-        </ProfileContainer>
-    )
-}
+      <HistoricContainer>
+        <P>Histórico de Pedidos</P>
+      </HistoricContainer>
 
-export default Profile
+      <Historic>Você não realizou nenhum pedido</Historic>
+    </ProfileContainer>
+  );
+};
+
+export default Profile;
