c458f9b131048562170cdb4f8f83c9c1a30f66f2
ArturCandelori <artur.candelori@hotmail.com>
2020-06-19 14:02:15 -0300

    fiz a passagem da feed para a página de restaurantes e a integração dela com a api

--- src/Pages/Restaurant/index.js
+++ src/Pages/Restaurant/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useProtectedPage } from "../../Hooks/ProtectedPage";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import CardProduct from "../../Components/CardProduct";
@@ -10,103 +10,139 @@ import {
   CardContent,
   CardMedia,
   makeStyles,
-  Container,
+  Container
 } from "@material-ui/core";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   header: {
     display: "flex",
     alignItems: "center",
     borderBottom: "1px solid gray"
   },
   button: {
-    marginRight: "20%",
+    marginRight: "20%"
   },
   card: {
     height: "30vh",
-    borderRadius: "10px",
+    borderRadius: "10px"
   },
   content: {
     display: "flex",
-    flexDirection: "column",
+    flexDirection: "column"
   },
   text: {
-    marginRight: "10%",
+    marginRight: "10%"
   },
   cover: {
-    height: 110,
-  },
+    height: 110
+  }
 }));
 
 const Restaurant = () => {
   const classes = useStyles();
   useProtectedPage();
+  const params = useParams();
+
+  const [restaurantDetails, setRestaurantDetails] = useState();
+
+  const getRestaurantDetails = () => {
+    axios
+      .get(
+        `https://us-central1-missao-newton.cloudfunctions.net/fourFoodB/restaurants/${
+          params.restaurantId
+        }`,
+        {
+          headers: {
+            auth: window.localStorage.getItem("token")
+          }
+        }
+      )
+      .then(response => {
+        console.log(response.data);
+        setRestaurantDetails(response.data.restaurant);
+      });
+  };
+
+  useEffect(() => {
+    getRestaurantDetails();
+    console.log(restaurantDetails);
+  }, []);
 
   return (
     <Container>
-      <Grid container spacing={2}>
-        <Grid item className={classes.header} xs={12}>
-          <IconButton className={classes.button}>
-            <ArrowBackIosIcon />
-          </IconButton>
-          <Typography variant='h5' align='center' component='span'>
-            Restaurante
-          </Typography>
+      {restaurantDetails === undefined ? (
+        <p>Carregando...</p>
+      ) : (
+        <Grid container spacing={2}>
+          <Grid item className={classes.header} xs={12}>
+            <IconButton className={classes.button}>
+              <ArrowBackIosIcon />
+            </IconButton>
+            <Typography variant="h5" align="center" component="span">
+              Restaurante
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.cover}
+                component="img"
+                image={restaurantDetails.logoUrl}
+                title="Logotipo"
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  color="primary"
+                  variant="h5"
+                  component="h2"
+                >
+                  {restaurantDetails.name}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {restaurantDetails.category}
+                </Typography>
+                <Typography
+                  className={classes.text}
+                  variant="body2"
+                  color="textSecondary"
+                  component="span"
+                >
+                  {restaurantDetails.deliveryTime} min
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="span"
+                >
+                  Frete: R$ {restaurantDetails.shipping}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {restaurantDetails.address}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item>
+            <Typography gutterBottom variant="h5" component="h2">
+              Refeição
+            </Typography>
+          </Grid>
+          {restaurantDetails.products.map(product => {
+            return (
+              <CardProduct
+                preco={"R$ " + product.price.toFixed(2)}
+                id={product.id}
+                foto={product.photoUrl}
+                nome={product.name}
+                descricao={product.desciption}
+              />
+            );
+          })}
         </Grid>
-        <Grid item>
-          <Card className={classes.card}>
-            <CardMedia
-              className={classes.cover}
-              component='img'
-              image='http://soter.ninja/futureFoods/logos/outback.png'
-              title='Logotipo Outback'
-            />
-            <CardContent>
-              <Typography
-                gutterBottom
-                color='primary'
-                variant='h5'
-                component='h2'
-              >
-                Outback Steakhouse
-              </Typography>
-              <Typography variant='body2' color='textSecondary' component='p'>
-                Carnes
-              </Typography>
-              <Typography
-                className={classes.text}
-                variant='body2'
-                color='textSecondary'
-                component='span'
-              >
-                20 min
-              </Typography>
-              <Typography
-                variant='body2'
-                color='textSecondary'
-                component='span'
-              >
-                Frete: R$ 18,00
-              </Typography>
-              <Typography variant='body2' color='textSecondary' component='p'>
-                "Alameda dos Marsupiais, 505 - Humaitá"
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item>
-          <Typography gutterBottom variant='h5' component='h2'>
-            Refeição
-          </Typography>
-        </Grid>
-        <CardProduct
-          preco='R$ 82,90'
-          id='1'
-          foto='https://static-images.ifood.com.br/image/upload/f_auto,t_high/pratos/5221af98-5ad4-42e2-a767-23d1545b82d5/201911191742_Cuuv_j.jpg'
-          nome='Jackaroo ribs & steak'
-          descricao='Exclusividade Outback'
-        />
-      </Grid>
+      )}
     </Container>
   );
 };
