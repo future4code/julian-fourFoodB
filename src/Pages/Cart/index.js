5519cf2de6641bc5308a4b3e2658825d0c4d81d0
ArturCandelori <artur.candelori@hotmail.com>
2020-06-18 23:08:58 -0300

    feito o mock do carrinho, não consegui colocar o número e remover

--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -11,18 +11,24 @@ import {
 } from '@material-ui/core';
 import { PrincipalButton } from '../../Themes';
 import { makeStyles } from "@material-ui/core/styles";
+import ProductCard from './ProductCard';
 
 const useStyles = makeStyles(theme => ({
+    root: {
+        height: "100vh"
+    },
     title: {
         padding: "12px"
     },
     address: {
         backgroundColor: "#eeeeee",
-        padding: "16px",
-        marginBottom: "16px"  
+        padding: "16px", 
+    },
+    empty: {
+        marginBottom: "45px"
     },
     payment: {
-        padding: "16px"
+        padding: "16px",
     },
     radioGroup: {
         width: "100vw"
@@ -30,11 +36,12 @@ const useStyles = makeStyles(theme => ({
     button: {
         backgroundColor: "#e8222e",
         height: "42px",
+        //width: "328px",
         borderRadius: "2px",
         textTransform: "none",
-        position: "absolute",
-        bottom: "32px",
-        right: "16px"
+        //position: "absolute",
+        //bottom: "16px",
+        //left: "16px",
     }
 }))
 
@@ -42,21 +49,33 @@ const Cart = () => {
     //useProtectedPage()
     const classes = useStyles();
     return(
-        <div>
+        <div className={classes.root}>
             <Typography className={classes.title}>Meu carrinho</Typography>
             <div className={classes.address}>
                 <Typography align="justify" gutterBottom color="textSecondary">Endereço de entrega</Typography>
                 <Typography align="justify" >Rua Alessandra Vieira, 42</Typography>
             </div>
-            <Typography>Carrinho vazio</Typography>
             <div className={classes.payment}>
+                {/* <Typography className={classes.empty}>Carrinho vazio</Typography> */}
+                <Typography align="justify" color="error" gutterBottom>Bullguer Villa Madalena</Typography>
+                <Typography align="justify" color="textSecondary" gutterBottom>R. Fradique Coutinho, 1136 - Vila Madalena</Typography>
+                <Typography align="justify" color="textSecondary" gutterBottom>30 - 45 min</Typography>
+                <Grid item container direction="column" spacing={2}>
+                    <Grid item>
+                        <ProductCard/>
+                    </Grid>
+                    <Grid item>
+                        <ProductCard/>
+                    </Grid>
+                </Grid>
+                
                 <Typography align="right">Frete R$0,00</Typography>
                 <Grid container justify="space-between">
                     <Grid item>
                         <Typography>SUBTOTAL</Typography>
                     </Grid>
                     <Grid item>
-                        <Typography variant="h6" color="error">R$0.00</Typography>
+                        <Typography variant="h6" color="error">R$00.00</Typography>
                     </Grid>
                 </Grid>
                 <Typography align="justify">Forma de pagamento</Typography>
@@ -64,12 +83,12 @@ const Cart = () => {
                 <form onSubmit="">
                 <FormControl component="fieldset" >
                     <RadioGroup className={classes.radioGroup}>
-                        <FormControlLabel value="dinheiro" control={<Radio />} label="Dinheiro"/>
-                        <FormControlLabel value="cartao" control={<Radio/>} label="Cartão de crédito"/>
+                        <FormControlLabel value="dinheiro" control={<Radio color="default"/>} label="Dinheiro"/>
+                        <FormControlLabel value="cartao" control={<Radio color="default"/>} label="Cartão de crédito"/>
                     </RadioGroup>
                     
                 </FormControl>
-                <Button disabled variant="contained" fullWidth className={classes.button} disableElevation>Confirmar</Button>
+                <Button variant="contained" fullWidth className={classes.button} disableElevation>Confirmar</Button>
                 </form>
             </div>
             
